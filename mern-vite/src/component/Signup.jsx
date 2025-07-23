a0773f775f93fb545175329506049e3f1c2c877d
Aryan Sharma <aryanlps9@gmail.com>
2025-07-23 20:32:42 +0530

    changes

--- mern-vite/src/component/Signup.jsx
+++ mern-vite/src/component/Signup.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import '../component/signup.css';
+import '../component/Signup.css';
 
 const Signup = () => {
   const [name, setName] = useState('');
